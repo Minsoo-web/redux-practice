246e454f1636caedede41cd1c362e838244a2a30
minsoo kim <zlemzlem5656@naver.com>
2021-07-02 21:35:27 +0900

    [추가] vanlia redux 마무리

--- src/index.js
+++ src/index.js
@@ -1,38 +1,75 @@
 import { createStore } from "redux";
 
-const add = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.getElementById("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-const ADD = "ADD";
-const MINUS = "MINUS";
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [{ text: action.text, id: action.id }, ...state];
+    case DELETE_TODO:
+      return state.filter(todo => todo.id !== action.id);
     default:
-      return count;
+      break;
   }
+  return state;
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-number.innerText = countStore.getState();
-
-add.addEventListener("click", () => {
-  countStore.dispatch({ type: ADD });
-});
-
-minus.addEventListener("click", () => {
-  countStore.dispatch({ type: MINUS });
-});
-
-const onChange = () => {
-  console.log("there was a change on the store");
-  number.innerText = countStore.getState();
+//
+const addTodo = text => {
+  return { type: ADD_TODO, text, id: Date.now() };
 };
 
-countStore.subscribe(onChange);
+const deleteTodo = id => {
+  return { type: DELETE_TODO, id };
+};
+
+//
+const dispatchAddTodo = text => {
+  store.dispatch(addTodo(text));
+};
+
+const dispatchDeleteTodo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteTodo(id));
+};
+
+const paintTodos = (old, newval) => {
+  const toDos = store.getState();
+  ul.innerHTML = ""; // 기존의 리스트를 다 지우고
+  // 다시 그린다.
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+
+    btn.innerText = "DEL";
+    btn.type = "button";
+
+    btn.addEventListener("click", dispatchDeleteTodo);
+
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+store.subscribe(paintTodos);
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddTodo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
